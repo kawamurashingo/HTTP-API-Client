85d8a167d0d1e4598e0513ac554b6eca6c368b61
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2026-08-16 17:32:28 +0900

    Add structured error class

---
+++ lib/HTTP/API/Client/Error.pm
@@ -0,0 +1,23 @@
+package HTTP::API::Client::Error;
+
+use strict;
+use warnings;
+use overload '""' => 'as_string', fallback => 1;
+
+sub new {
+    my ($class, %args) = @_;
+    return bless \%args, $class;
+}
+
+sub message     { $_[0]->{message} }
+sub category    { $_[0]->{category} }
+sub status      { $_[0]->{status} }
+sub method      { $_[0]->{method} }
+sub url         { $_[0]->{url} }
+sub retryable   { $_[0]->{retryable} ? 1 : 0 }
+sub retry_after { $_[0]->{retry_after} }
+sub request_id  { $_[0]->{request_id} }
+sub response    { $_[0]->{response} }
+sub as_string   { defined($_[0]->{message}) ? $_[0]->{message} : 'HTTP API client error' }
+
+1;
