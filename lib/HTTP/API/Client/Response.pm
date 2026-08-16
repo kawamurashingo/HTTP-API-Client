8f75e91b2ea65178a232af1b00bbc945b3a2974b
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2026-08-16 17:32:34 +0900

    Add structured response class

---
+++ lib/HTTP/API/Client/Response.pm
@@ -0,0 +1,45 @@
+package HTTP::API::Client::Response;
+
+use strict;
+use warnings;
+use JSON::PP qw(decode_json);
+use HTTP::API::Client::Error;
+
+sub new {
+    my ($class, %args) = @_;
+    return bless {
+        status  => $args{status},
+        reason  => $args{reason},
+        headers => { map { lc($_) => $args{headers}{$_} } keys %{ $args{headers} || {} } },
+        content => defined($args{content}) ? $args{content} : '',
+        method  => $args{method},
+        url     => $args{url},
+    }, $class;
+}
+
+sub status  { $_[0]->{status} }
+sub reason  { $_[0]->{reason} }
+sub headers { +{ %{ $_[0]->{headers} } } }
+sub content { $_[0]->{content} }
+sub method  { $_[0]->{method} }
+sub url     { $_[0]->{url} }
+sub is_success { $_[0]->{status} >= 200 && $_[0]->{status} < 300 }
+sub header { my ($self, $name) = @_; return $self->{headers}{lc $name} }
+
+sub json {
+    my ($self) = @_;
+    my $decoded = eval { decode_json($self->{content}) };
+    if ($@) {
+        die HTTP::API::Client::Error->new(
+            category   => 'decode',
+            status     => $self->{status},
+            method     => $self->{method},
+            url        => $self->{url},
+            response   => $self,
+            message    => "failed to decode JSON response: $@",
+        );
+    }
+    return $decoded;
+}
+
+1;
